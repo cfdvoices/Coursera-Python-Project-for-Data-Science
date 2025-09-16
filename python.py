19e5a45b292f6b99a9e33f0b2da2bc6449dc0cc7
cfdvoices <159398503+cfdvoices@users.noreply.github.com>
2025-09-16 14:04:51 -0500

    Create python.py

---
+++ python.py
@@ -0,0 +1,17 @@
+# Reset the index
+# Reset the index
+
+import yfinance as yf
+import pandas as pd
+
+# Download Tesla stock data
+tesla_data = yf.download('TSLA')
+
+# Reset the index
+tesla_data.reset_index(inplace=True)
+
+# Display the first five rows
+print(tesla_data.head())
+
+
+
